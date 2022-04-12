b535f93985878d92cbbec24a7980cde2e09ac324
Quang Nguyen <quangnm.work@gmail.com>
2022-04-12 12:26:42 +0700

    add navigation task

--- src/pages/control/OverallLayout.tsx
+++ src/pages/control/OverallLayout.tsx
@@ -1,9 +1,11 @@
 import { Routes } from 'react-router-dom';
+import Navigation from '../../components/Navigation/Navigation';
 import RenderRoutes from '../../utils/RenderRoutes';
 
 import AuthRoutes from '../authentication/routes';
 const OverallLayout = () => {
-  return <Routes>{RenderRoutes(AuthRoutes)}</Routes>;
+  // return <Routes>{RenderRoutes(AuthRoutes)}</Routes>;
+  return <Navigation />;
 };
 
 export default OverallLayout;
