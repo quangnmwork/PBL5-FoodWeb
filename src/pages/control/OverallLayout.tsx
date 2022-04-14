20644db171a98fe5b4439c1677e3f27b4c57c5ce
Quang Nguyen <quangnm.work@gmail.com>
2022-04-14 23:22:31 +0700

    update fe-dev-test

--- src/pages/control/OverallLayout.tsx
+++ src/pages/control/OverallLayout.tsx
@@ -1,5 +1,4 @@
 import { Routes } from 'react-router-dom';
-import Navigation from '../../components/Navigation/Navigation';
 import RenderRoutes from '../../utils/RenderRoutes';
 
 import AuthRoutes from '../authentication/routes';
