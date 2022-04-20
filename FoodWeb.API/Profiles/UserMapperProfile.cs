07137c94bed0a9a5deb731a9cae9993deefb0e5c
= <=>
2022-04-20 15:06:30 +0700

    fix error map OrderDetail -> OrderDTO

--- FoodWeb.API/Profiles/UserMapperProfile.cs
+++ FoodWeb.API/Profiles/UserMapperProfile.cs
@@ -28,7 +28,8 @@ namespace FoodWeb.API.Profiles
             CreateMap<OrderDetail, OrderDTO>().ForMember(dest => dest.IdRoom, opt => opt.MapFrom(src => src.Room.IdRoom))
                                               .ForMember(dest => dest.IdPayment, opt => opt.MapFrom(src => src.Payment.IdPayment))
                                               .ForMember(dest => dest.IdCustomer, opt => opt.MapFrom(src => src.Customer.IdUser))
-                                              .ForMember(dest => dest.IdShipper, opt => opt.MapFrom(src => src.Shipper.IdUser));
+                                              //.ForMember(dest => dest.IdShipper, opt => opt.MapFrom(src => src.Shipper.IdUser))
+                                              .ForMember(dest => dest.IdShipper, opt => opt.MapFrom(src => src.ShipperId));
 
             CreateMap<ListOrder, InfoFoodOrderDTO>().ForMember(dest => dest.IdFood, opt => opt.MapFrom(src => src.FoodId))
                                                     .ForMember(dest => dest.NameFood, opt => opt.MapFrom(src => src.Food.NameFood))
