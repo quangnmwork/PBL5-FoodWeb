7df5e3654b9e4c6bbbc0447d1eb950b41440b4aa
Ngọc Hoàng Minh Hồ <hongochoangm@gmail.com>
2022-04-24 10:29:14 +0700

    Add Cloudinary Service

--- FoodWeb.API/Controllers/UsersController.cs
+++ FoodWeb.API/Controllers/UsersController.cs
@@ -6,8 +6,6 @@ using System.Net;
 using System.Net.Mime;
 using System.Security.Claims;
 using AutoMapper.Internal;
-using CloudinaryDotNet;
-using CloudinaryDotNet.Actions;
 using FoodWeb.API.Database;
 using FoodWeb.API.Database.Entities;
 using FoodWeb.API.Database.IRepositories;
@@ -29,23 +27,17 @@ namespace FoodWeb.API.Controllers
         private readonly IUserRepository _userRepository;
         private readonly IAuthorizeService _authorizeService;
         private readonly IFoodRepository _foodRepository;
-        private readonly Cloudinary _cloudinary;
+        private readonly ICloudinaryService _cloudinary;
 
         public UsersController(IUserRepository userRepository,
                                IAuthorizeService authorizeService,
                                IFoodRepository foodRepository,
-                               IConfiguration config)
+                               ICloudinaryService cloudinary)
         {
             this._foodRepository = foodRepository;
             this._userRepository = userRepository;
             this._authorizeService = authorizeService;
-            // Console.WriteLine(config.GetValue<String>("CLOUD_NAME") + config.GetValue<String>("API_KEY"));
-            CloudinaryDotNet.Account account = new CloudinaryDotNet.Account(
-                config.GetValue<String>("CLOUD_NAME"),
-                config.GetValue<String>("API_KEY"),
-                config.GetValue<String>("API_SECRET")
-            );
-            this._cloudinary = new Cloudinary(account);
+            this._cloudinary = cloudinary;
         }
 
         [HttpGet("GetProfileUser")]   //Lấy profile của user
@@ -80,27 +72,11 @@ namespace FoodWeb.API.Controllers
             var avatarFile = HttpContext.Request.Form.Files.GetFile("Avatar");
             if (avatarFile!=null){
                 // Console.WriteLine("Lenght " +avatarFile.Length);
-                userDTO.Avatar = UploadImage(avatarFile);
+                userDTO.Avatar = _cloudinary.UploadImage("Users",avatarFile);
                 // Console.WriteLine("Link " +userDTO.Avatar);
             }
             _userRepository.UpdateProfile(Int32.Parse(Id), userDTO);
             return Ok(_userRepository.GetProfileUserById(Int32.Parse(Id)));
-        }
-        public String UploadImage(IFormFile avatarFile)
-        {
-            var ms = new MemoryStream();
-            avatarFile.CopyTo(ms);
-            var fileBytes = ms.ToArray();
-            ms = new MemoryStream(fileBytes, 0, fileBytes.Length);
-
-            ImageUploadParams uploadParams = new ImageUploadParams()
-            {
-                File = new FileDescription("file", ms),
-                PublicId = "Users/" + avatarFile.FileName + DateTime.Now
-            };
-
-            ImageUploadResult result = _cloudinary.Upload(uploadParams);
-            return _cloudinary.Api.UrlImgUp.BuildUrl(String.Format("{0}.{1}", result.PublicId, result.Format));
         }
 
         [HttpGet("getTotalPageAllSellers")] //Lấy tổng số trang của list seller
