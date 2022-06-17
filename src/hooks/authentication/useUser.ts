5c321ccd7c4870fdbfa798ed45d2f5c01d40e561
Quang Nguyen <quangnm.work@gmail.com>
2022-06-17 11:21:50 +0700

    update user hook logic

--- src/hooks/authentication/useUser.ts
+++ src/hooks/authentication/useUser.ts
@@ -2,20 +2,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import useSWR from 'swr';
-import clientStorage from '../../utils/clientStorage';
 
 import { User } from './../../models/User.model';
 
 export const useUser = (timeRefreshInterval?: number) => {
-  let condition = false;
-  if (clientStorage.getClientStorage().getToken()) {
-    condition = true;
-  }
-
   const options = timeRefreshInterval ? timeRefreshInterval : 0;
 
   const { data, error, mutate, isValidating } = useSWR<User>(
-    condition ? `${process.env.REACT_APP_DOMAIN}Users/GetProfileUser` : null,
+    `${process.env.REACT_APP_DOMAIN}Users/GetProfileUser`,
     { refreshInterval: options }
   );
 
